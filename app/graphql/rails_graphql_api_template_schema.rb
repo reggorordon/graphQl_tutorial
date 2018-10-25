9914ac22ac946e85584b139759298aff6aec564e
Reg Gordon <reg.gordon@shopify.com>
2018-10-25 08:39:25 +0100

    corrected an issue that prevented graphiql running if no mutations are present

--- app/graphql/rails_graphql_api_template_schema.rb
+++ app/graphql/rails_graphql_api_template_schema.rb
@@ -1,4 +1,6 @@
 class RailsGraphqlApiTemplateSchema < GraphQL::Schema
-  mutation(Types::MutationType)
+  # mutation needs to be commented out to prevent graphiql failing to run once queries are added. 
+  # once you have created a mutation you can uncomment it 
+ # mutation(Types::MutationType)
   query(Types::QueryType)
 end
